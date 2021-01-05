dd26ec9bbe9d371ded34add00c468be78cbb3b74
jluis0007 <jluis.0007@hotmail.com>
2021-01-04 21:48:58 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://jluis:R6e76EEU9v8GznYm@cluster0.mvaxx.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
